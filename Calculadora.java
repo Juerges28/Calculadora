262a1b2de47960b008f67813c8f9c2929e3eb62e
Jorge Luis Escobedo Ocaña <jescobedooc@unsa.edu.pe>
2023-05-05 12:05:15 -0500

    Calculadora.java base

---
+++ Calculadora.java
@@ -0,0 +1,26 @@
+public class Calculator()
+{
+	public static void main(String args[]){
+	Scanner sc = new Scanner(System.in);
+	double a,b;
+	int eleccion;
+	System.out.print("Ingrese el primer numero: ");
+	a=sc.nextDouble();
+	System.out.print("Ingrese el  segundo numero: ");
+        b=sc.nextDouble();
+	System.out.print("Seleccione la operacion: ");
+        eleccion=sc.nextInt();
+	switch(eleccion){
+	case(1): System.out.print(add(a,b));
+		 break;
+	case(2): System.out.print(sub(a,b));
+                 break;
+	case(3): System.out.print(mul(a,b));
+                 break;
+	case(4): System.out.print(div(a,b));
+                 break;
+	case(5): System.out.print(mod(a,b));
+	}
+	}
+
+}
