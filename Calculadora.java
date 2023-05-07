3bcc6ee3040ce1887e3b85bbc57d6993646f8dc3
Jorge Luis Escobedo Ocaña <jescobedooc@unsa.edu.pe>
2023-05-06 19:12:17 -0500

    Metodo div agregado

--- Calculadora.java
+++ Calculadora.java
@@ -1,26 +1,28 @@
 public class Calculator()
 {
 	public static void main(String args[]){
-	Scanner sc = new Scanner(System.in);
-	double a,b;
-	int eleccion;
-	System.out.print("Ingrese el primer numero: ");
-	a=sc.nextDouble();
-	System.out.print("Ingrese el  segundo numero: ");
-        b=sc.nextDouble();
-	System.out.print("Seleccione la operacion: ");
-        eleccion=sc.nextInt();
-	switch(eleccion){
-	case(1): System.out.print(add(a,b));
-		 break;
-	case(2): System.out.print(sub(a,b));
-                 break;
-	case(3): System.out.print(mul(a,b));
-                 break;
-	case(4): System.out.print(div(a,b));
-                 break;
-	case(5): System.out.print(mod(a,b));
+		Scanner sc = new Scanner(System.in);
+		double a,b;
+		int eleccion;
+		System.out.print("Ingrese el primer numero: ");
+		a=sc.nextDouble();
+		System.out.print("Ingrese el  segundo numero: ");
+        	b=sc.nextDouble();
+		System.out.print("Seleccione la operacion: ");
+        	eleccion=sc.nextInt();
+		switch(eleccion){
+		case(1): System.out.print(add(a,b));
+			 break;
+		case(2): System.out.print(sub(a,b));
+                	 break;
+		case(3): System.out.print(mul(a,b));
+        	         break;
+		case(4): System.out.print(div(a,b));
+        	         break;
+		case(5): System.out.print(mod(a,b));
+		}
 	}
+	public static double div(double a, double b){
+		return a/b;	
 	}
-
 }
